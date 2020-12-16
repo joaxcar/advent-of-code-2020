0d7f06b9a6c0ad6bbc2194438a581c948f931a1a
Johan Carlsson <johan.axel.carlsson@gmail.com>
2020-12-16 09:54:33 +0100

    Add day 14 solution

--- Day14/Main.hs
+++ Day14/Main.hs
@@ -1,31 +1,71 @@
-import qualified Control.Monad.State as C
-import qualified Control.Monad.State as S
-import Data.Functor.Identity
 import Data.IntMap (IntMap)
 import qualified Data.IntMap.Strict as M
 import Text.Parsec
-import Text.Parsec.String (Parser)
+  ( digit,
+    eof,
+    many1,
+    oneOf,
+    parse,
+    sepBy,
+    string,
+    try,
+  )
 import Text.Printf (printf)
 
 main = do
-  input <- readFile "Day14/data.txt"
+  input <- readFile "data.txt"
   let prog = getRight $ parse cmds "" input
-  putStrLn . show $ runIt prog
+  print . task2 . M.unions . reverse $ map (mem . runner) prog
 
-noMask = map toTernary "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+-- Modeling the program
+data ProgState = ProgState {mem :: IntMap Bin36, mask :: Mask}
 
-applyMask [] _ = []
-applyMask (m : ms) (b : bs) | m == MaskNone = b : applyMask ms bs
-applyMask (m : ms) (b : bs) | m == MaskOne = One : applyMask ms bs
-applyMask (m : ms) (b : bs) | m == MaskZero = Zero : applyMask ms bs
+data Binary = Zero | One deriving (Show, Eq, Read, Ord, Bounded)
 
-toDec l = foldl (\acc x -> if x == Zero then 2 * acc else 2 * acc + 1) 0 l
+data Ternary = MaskZero | MaskOne | MaskNone deriving (Show, Eq)
 
-runIt k = C.evalState (runner k) (ProgState M.empty noMask)
+type Bin36 = [Binary]
 
+type Mask = [Ternary]
+
+data Cmd = BinData {addr :: Int, val :: Bin36} deriving (Show, Eq)
+
+task2 :: IntMap [Binary] -> Integer
+task2 = sum . map toDec . M.elems
+
+runner :: Foldable t => (Mask, t Cmd) -> ProgState
+runner (mask, l) = foldl runns (ProgState M.empty mask) l
+  where
+    runns (ProgState mem m) (BinData addr val) =
+      let addrs = applyMask m (map toBinary $ intToBinString addr)
+          add x acc = M.insert (toDec x) val acc
+          newMem = foldr add mem addrs
+       in ProgState newMem mask
+
+runner1 (mask, l) = foldl runns (ProgState M.empty mask) l
+  where
+    runns (ProgState mem mask) (BinData addr val) =
+      let newMem = M.insert addr (applyMask mask val) mem
+       in ProgState newMem mask
+
+applyMask :: [Ternary] -> [Binary] -> [[Binary]]
+applyMask m b = applyMask' m b [[]]
+
+applyMask' :: [Ternary] -> [Binary] -> [[Binary]] -> [[Binary]]
+applyMask' [] _ a = a
+applyMask' (m : ms) (b : bs) acc
+  | m == MaskNone = applyMask' ms bs ((:) <$> [One, Zero] <*> acc)
+  | m == MaskOne = applyMask' ms bs ((:) <$> [One] <*> acc)
+  | m == MaskZero = applyMask' ms bs ((:) <$> [b] <*> acc)
+
+toDec :: (Foldable t, Num a) => t Binary -> a
+toDec = foldl (\acc x -> if x == Zero then 2 * acc else 2 * acc + 1) 0
+
+getRight :: Either a1 [a2] -> [a2]
 getRight (Right a) = a
 getRight (Left _) = []
 
+toTernary :: Char -> Ternary
 toTernary x
   | x == '0' = MaskZero
   | x == '1' = MaskOne
@@ -36,50 +76,26 @@ toBinary x
   | x == '0' = Zero
   | x == '1' = One
 
-runner :: [Cmd] -> C.State ProgState Int
-runner [] = do
-  ProgState mem val <- C.get
-  return $ sum . map toDec $ M.elems mem
-runner ((BinData addr val) : res) = do
-  ProgState mem mask <- C.get
-  let newMem = M.insert addr (applyMask mask val) mem
-  C.put $ ProgState newMem mask
-  runner res
-runner ((NewMask newMask) : res) = do
-  ProgState mem _ <- C.get
-  C.put $ ProgState mem newMask
-  runner res
-
-data ProgState = ProgState {mem :: IntMap Bin36, mask :: [Ternary]}
-
-data Binary = Zero | One deriving (Show, Eq, Read, Ord, Bounded)
-
-data Ternary = MaskZero | MaskOne | MaskNone deriving (Show, Eq)
-
-type Bin36 = [Binary]
-
-type Mask = [Ternary]
-
-data Cmd = BinData {addr :: Int, val :: Bin36} | NewMask Mask deriving (Show, Eq)
+intToBinString n = printf "%036b" n :: String
 
 -- parser
 
 getMask = do
-  string "ask = "
+  string "mask = "
   m <- many1 (oneOf "X10")
-  return (NewMask $ map toTernary m)
+  return (map toTernary m)
 
 getMemory = do
-  string "em["
+  string "["
   addr <- read <$> many1 digit
   string "] = "
   val <- (\x -> read x :: Int) <$> many1 digit
-  return (BinData addr (map toBinary . printf "%036b" $ val))
+  return (BinData addr (map toBinary . intToBinString $ val))
 
-command :: ParsecT String u Identity Cmd
 command = do
-  char 'm'
-  getMask <|> getMemory
+  mask <- getMask
+  string "\nmem"
+  mems <- sepBy getMemory (try (string "\nmem"))
+  return (mask, mems)
 
-cmds :: ParsecT String u Identity [Cmd]
-cmds = sepBy command (char '\n') <* eof
+cmds = sepBy command (string "\n") <* eof
